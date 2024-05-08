c3167b79fb98a8d2f772f84b7db05854de00f06a
user.name <akamrannicole@gmail.com>
2024-05-08 19:06:33 +0300

    HomePage component updated

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import Navbar from "./Navbar";
 
-function HomePage (){
+function HomePage() {
+  const [recipes, setRecipes] = useState([]);
 
-return(
+  useEffect(() => {
+    fetch('/home/shelvin/developments/REACT/Cookbook-companion/db.json')
+      .then(response => response.json())
+      .then(data => setRecipes(data))
+      .catch(error => console.error('Error fetching recipes:', error));
+  }, []);
+
+  return (
     <div>
-   <Navbar/>
-
+      <Navbar />
+      <div className="carousel-container">
+        <Carousel showArrows={true} showStatus={false} showThumbs={false}>
+          {recipes.map((recipe, index) => (
+            <div key={index} className="recipe">
+              <h2>{recipe.title}</h2>
+              <p>{recipe.description}</p>
+              <img src={recipe.image} alt={recipe.title} />
+            </div>
+          ))}
+        </Carousel>
+      </div>
     </div>
-);
-
+  );
 }
 
 export default HomePage;
