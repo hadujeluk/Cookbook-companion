716e2e644def596c40d0ba07aa7a803f2bdc801a
user.name <akamrannicole@gmail.com>
2024-05-09 01:18:55 +0300

    HomePage styled

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,31 +1,51 @@
 import React, { useState, useEffect } from "react";
-import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import Navbar from "./Navbar";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./HomePage.css"; // Import the CSS file
 
 function HomePage() {
   const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
-    fetch('/home/shelvin/developments/REACT/Cookbook-companion/db.json')
-      .then(response => response.json())
-      .then(data => setRecipes(data))
-      .catch(error => console.error('Error fetching recipes:', error));
+    // Fetch recipe data from db.json or an API endpoint
+    fetch("db.json")
+      .then((response) => response.json())
+      .then((data) => setRecipes(data.recipes))
+      .catch((error) => console.error("Error fetching data:", error));
   }, []);
 
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: true
+  };
+
   return (
-    <div>
-      <Navbar />
+    <div className="homepage">
+      <div className="homepage-header">
+        <h1 className="header">A Chef In Every Tasty Meal Box</h1>
+        <p className="paragraph">Explore a world of flavors and unleash your culinary creativity with our delightful collection of recipes. From quick and easy meals to gourmet delights, we've got something for every taste bud.</p>
+        <div className="buttons-container">
+          <button className="button-primary">Explore Recipes</button>
+          <button className="button-secondary">Learn More</button>
+        </div>
+      </div>
       <div className="carousel-container">
-        <Carousel showArrows={true} showStatus={false} showThumbs={false}>
-          {recipes.map((recipe, index) => (
-            <div key={index} className="recipe">
+        <Slider {...settings}>
+          {recipes.map((recipe) => (
+            <div key={recipe.id}>
+              <img src={recipe.image} alt={recipe.title} />
               <h2>{recipe.title}</h2>
               <p>{recipe.description}</p>
-              <img src={recipe.image} alt={recipe.title} />
             </div>
           ))}
-        </Carousel>
+        </Slider>
       </div>
     </div>
   );
