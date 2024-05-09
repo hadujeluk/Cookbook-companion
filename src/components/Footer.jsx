a7bae473469cb06765c7542b8bb545790289b9d7
user.name <akamrannicole@gmail.com>
2024-05-09 04:12:29 +0300

    footer is done getting updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,16 +16,16 @@ function Footer() {
     <footer className="footer">
       <div className="footer-container">
         <div className="social-icons">
-          <a href="https://www.facebook.com/your-facebook-page" target="_blank" rel="noopener noreferrer">
+          <a href="#facebook">
             <FontAwesomeIcon icon={faFacebookF} className="icon" />
           </a>
-          <a href="https://twitter.com/your-twitter-handle" target="_blank" rel="noopener noreferrer">
+          <a href="#twitter">
             <FontAwesomeIcon icon={faTwitter} className="icon" />
           </a>
-          <a href="https://www.instagram.com/your-instagram-handle" target="_blank" rel="noopener noreferrer">
+          <a href="#instagram">
             <FontAwesomeIcon icon={faInstagram} className="icon" />
           </a>
-          <a href="https://www.linkedin.com/company/your-linkedin-page" target="_blank" rel="noopener noreferrer">
+          <a href="#linkedin">
             <FontAwesomeIcon icon={faLinkedinIn} className="icon" />
           </a>
         </div>
@@ -33,23 +33,33 @@ function Footer() {
           <div className="link-group">
             <h3>Support</h3>
             <ul>
-              <li><a href="/faqs">FAQs</a></li>
-              <li><a href="/help-center">Help Center</a></li>
+              <li><a href="#faqs">FAQs</a></li>
+              <li><a href="#help-center">Help Center</a></li>
             </ul>
           </div>
           <div className="link-group">
             <h3>Information</h3>
             <ul>
-              <li><a href="/about-us">About Us</a></li>
-              <li><a href="/policy">Policy</a></li>
+              <li><a href="#about-us">About Us</a></li>
+              <li><a href="#policy">Policy</a></li>
             </ul>
           </div>
           <div className="link-group">
             <h3>Feedback & Request</h3>
             <ul>
-              <li><a href="/feedback">Feedback Form</a></li>
+              <li><a href="#feedback">Feedback Form</a></li>
             </ul>
           </div>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        <div className="signature">
+          <span>© 2024 Cookbook Companion</span>
+        </div>
+        <div className="extra-info">
+          <a href="#faqs"><FontAwesomeIcon icon={faQuestionCircle} className="footer-icon" /></a>
+          <a href="#about-us"><FontAwesomeIcon icon={faInfoCircle} className="footer-icon" /></a>
+          <a href="#feedback"><FontAwesomeIcon icon={faCommentAlt} className="footer-icon" /></a>
         </div>
       </div>
     </footer>
