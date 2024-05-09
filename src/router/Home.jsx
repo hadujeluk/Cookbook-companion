1bc0e0a485d79865cd54162f7134005fac302207
user.name <akamrannicole@gmail.com>
2024-05-09 11:17:00 +0300

    Updated the main component

--- src/router/Home.jsx
+++ src/router/Home.jsx
@@ -1,19 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import HomePage from '../components/HomePage';
-
-
+import AboutPage from '../components/AboutPage'; 
 
 function Home() {
   return (
-    <div>
+    <Router>
+      <div>
         <Navbar />
-        <HomePage/>
-        
-        
-    
-  </div>
-);
+        <Switch>
+          <Route path="/" exact>
+            <HomePage />
+          </Route>
+          <Route path="/about">
+            <AboutPage />
+          </Route>
+          
+        </Switch>
+      </div>
+    </Router>
+  );
 }
-  
+
 export default Home;
