1af00efc8994e85156e41febf7d2cbeca3711bee
kennedy <kitongakennedy9@gmail.com>
2024-05-10 19:56:34 +0300

    added a recipe

--- src/router/route.jsx
+++ src/router/route.jsx
@@ -8,39 +8,31 @@ import RecipeDetails from "../components/recipeDetails";
 import Footer from "../components/Footer";
 import RenderDetails from "../components/renderDetails";
 
-
 const route = createBrowserRouter([
-    {
-        path: '/',
-        element: <Home />
-    },
-    {
-     path: '/about',
-     element: <About/>
-
-    },
-    {
-  path:'/recipes',
-  element:<RecipeCollection/>
-
-    },
-    {
-      path: '/details/:id',
-      element: <RecipeDetails />
-    },
-    {
-      path:'/footer',
-      element: <Footer />
-    },
-    {
-      path:'/recipes',
-      element:<RenderDetails/>
-    
-        }
-
-   
-]
-    
-)
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/recipes",
+    element: <RecipeCollection />,
+  },
+  {
+    path: "/details/:id",
+    element: <RecipeDetails />,
+  },
+  {
+    path: "/footer",
+    element: <Footer />,
+  },
+  {
+    path: "/recipes",
+    element: <RenderDetails />,
+  },
+]);
 
 export default route;
